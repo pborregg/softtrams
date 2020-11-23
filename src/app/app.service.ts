b353aa8fe93a53674b150822e05cba8e8cf9722f
pborregg <pborregg>
2020-11-22 21:12:13 -0800

    reformatted to 4 tabs

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,37 +4,37 @@ import { catchError } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AppService {
-  api = 'http://localhost:8000/api';
-  username: string;
+	api = 'http://localhost:8000/api';
+	username: string;
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
-  // Returns all members
-  getMembers() {
-    return this.http
-      .get(`${this.api}/members`)
-      .pipe(catchError(this.handleError));
-  }
+	// Returns all members
+	getMembers() {
+		return this.http
+			.get(`${this.api}/members`)
+			.pipe(catchError(this.handleError));
+	}
 
-  setUsername(name: string): void {
-    this.username = name;
-  }
+	setUsername(name: string): void {
+		this.username = name;
+	}
 
-  addMember(memberForm) {}
+	addMember(memberForm) { }
 
-  getTeams() {}
+	getTeams() { }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('An error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-    }
-    return [];
-  }
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+			console.error('An error occurred:', error.error.message);
+		} else {
+			console.error(
+				`Backend returned code ${error.status}, ` + `body was: ${error.error}`
+			);
+		}
+		return [];
+	}
 }
